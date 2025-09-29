79756501cc36b70886c418709fa5e122da8d2f27
Tuan Duy <tranquoctuanduy@gmail.com>
2025-09-29 16:54:25 +0700

    Xây dựng giao diện homepage

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,13 +1,13 @@
 import './Login.css'
 function Login() {
-    
+
     return (
         <div className="container">
             <h2>Sign in or Sign up</h2>
             <p>Express yourself, connect with others.</p>
             <button className="btn">Sign in</button>
             <button className="btn">Sign up</button><br/>
-            <button onClick className="link">Continue as guest</button>
+            <a href='#' >Continue as guest</a>
         </div>
     );
 }
