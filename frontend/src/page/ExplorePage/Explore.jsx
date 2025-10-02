b5373c9bb27b287dbfbc2ab541af46d9f6ce57a3
Tuan Duy <tranquoctuanduy@gmail.com>
2025-10-02 08:39:17 +0700

    Sửa đổi thanh tìm kiếm trong Explore

--- frontend/src/page/ExplorePage/Explore.jsx
+++ frontend/src/page/ExplorePage/Explore.jsx
@@ -3,7 +3,14 @@ export default function Explore(){
     return(
         <>
             <div className={style["box-explore"]}>
-                <input className={style["search-bar"]} type="text" placeholder="      Tìm kiếm" />
+                <div className={style["search-container"]}>
+                    <i className="fa fa-search"></i>
+                    <input
+                        className={style["search-bar"]}
+                        type="text"
+                        placeholder="Tìm kiếm"
+                    />
+                </div>
             </div>
         </>
     )
