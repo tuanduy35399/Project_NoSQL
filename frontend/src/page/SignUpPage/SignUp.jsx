916100f1462dbec15578d602d8f3ea9fa638415a
thoconomcarot <nguyennhuquynhct2005@gmail.com>
2025-10-23 23:13:45 +0700

    update

--- frontend/src/page/SignUpPage/SignUp.jsx
+++ frontend/src/page/SignUpPage/SignUp.jsx
@@ -1,22 +1,44 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import "./SignUp.css";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const [fullName, setFullName] = useState("");
   const [userName, setUserName] = useState("");
   const [birthday, setBirthday] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate(); //dùng để chuyển page sau khi đky thành công
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({
-      fullName,
-      userName,
-      birthday,
-      password,
-    });
+  const handleSubmit = async (e) => {  //gọi hàm xử lý khi nhấn đăng ký
+    // vì dùng axios nên cần async await
+    e.preventDefault();  // tránh reload page khi submit form
+
+    const formData = {fullName, userName, birthday, password}; //tạo object chứa data form
+
+    try {
+      const response = await axios.post("http://localhost:8080/api/users", formData, {  // gửi dữ liệu lên server
+        //headers: { "Content-Type": "multipart/form-data" },  //Khai báo kdl gửi đi
+        //do SignUp không cần upload file nên không cần khai báo kdl formdata này vì chỉ cần file json là được 
+        //axios sẽ tự động thêm header phù hợp
+      });
+
+      console.log("Signup success:", response.data);  // xử lý phản hồi khi thành công
+      alert("Signup successfully! Please sign in.");
+      navigate("/signin");  //chuyển đến trang đăng nhập sau khi đky thành công
+    } catch (error) {
+      console.error("Signup failed:", error);
+      alert("Signup failed! Please try again.");
+    }
   };
+
+  //chỉ để kiểm tra form trước khi gửi (debug)
+  console.log({
+    fullName,
+    userName,
+    birthday,
+    password,
+  });
 
   return (
     <div className="signup-container">
